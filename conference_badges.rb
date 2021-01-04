0500f6988b16b70323e255e411882967ebf6ceb7
Shami Tomita <shamitomita@gmail.com>
2021-01-04 18:35:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ end
 def assign_rooms(array)
   assigned_rooms = []
   array.each_with_index do |name, index|
-    x = "Hello, #{name}! Youll be assigned to room #{index+1}!"
+    x = "Hello, #{name}! You'll be assigned to room #{index+1}!"
     assigned_rooms.push(x)
   end
   return assigned_rooms
