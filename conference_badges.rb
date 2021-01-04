604b40939e2b5c17b544c69256fc05c7c013257a
Shami Tomita <shamitomita@gmail.com>
2021-01-04 18:16:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+array = ["Baby", "Cookie", "Gila Monster"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  
+end 
+
+def assign_rooms(array)
+end 
+
+def printer
+end 
