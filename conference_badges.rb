7485e18a98d6ca4f42596635c5bc4a522b0acd62
Shami Tomita <shamitomita@gmail.com>
2021-01-04 18:18:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,11 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array)
-  
+  badges = []
+  array.each do |name|
+   x = badge_maker(name)
+ end 
+   badges.shift(x)
 end 
 
 def assign_rooms(array)
